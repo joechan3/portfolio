e81fcd692d32ddd23100fc3ce7bbf1688121f09e
Joseph Chan <joechan3@gmail.com>
2016-04-10 19:49:44 -0600

    Minor tweaks

--- js/responsive-nav.js
+++ js/responsive-nav.js
@@ -196,7 +196,7 @@
          * @type {Object}
          */
         this.options = {
-          animate: true,                    // Boolean: Use CSS3 transitions, true or false
+          animate: false,                    // Boolean: Use CSS3 transitions, true or false
           transition: 284,                  // Integer: Speed of the transition, in milliseconds
           label: "Menu",                    // String: Label for the navigation toggle
           insert: "after",                 // String: Insert the toggle before or after the navigation
